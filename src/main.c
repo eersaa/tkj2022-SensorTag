e55dc1ceeec5719070bf14cae98f93bc270852b5
eersaa <eerik.saarinen@gmail.com>
2022-11-18 18:57:07 +0000

    Add example code to call get_pet_data()

--- src/main.c
+++ src/main.c
@@ -1,8 +1,31 @@
+
+#include <unistd.h> // For Sleep() to work on linux
+//#include <windows.h> // For Sleep() to work on windows
+#include <stdio.h>
+
 #include "mpuSim.h"
 
 
 int main(int argc, char const *argv[])
 {
+    // Initialize table
     int t = init_data();
-    return t;
+    // Check for errors
+    if (t)
+    {
+        return t;
+    }
+    
+    float ax, ay, az, gx, gy, gz;
+
+    // Loop forever
+    while (1)
+    {
+        get_pet_data(&ax, &ay, &az, &gx, &gy, &gz);
+
+        printf("Data: %4.2f,%4.2f,%4.2f,%4.2f,%4.2f,%4.2f\n", ax, ay, az, gx, gy, gz);
+
+        sleep(1);
+    }
+    
 }
