c1fb18d6cd9dcbe8206b1fff716e455b130743be
eersaa <eerik.saarinen@gmail.com>
2022-11-19 11:12:07 +0000

    Add example call for get_exe_data()

--- src/main.c
+++ src/main.c
@@ -25,6 +25,8 @@ int main(int argc, char const *argv[])
         get_pet_data(&ax, &ay, &az, &gx, &gy, &gz);
         // Call eat data
         get_eat_data(&ax, &ay, &az, &gx, &gy, &gz);
+        // Call exercise data
+        get_exe_data(&ax, &ay, &az, &gx, &gy, &gz);
 
         sleep(1);
     }
